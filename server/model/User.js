71340050ce1c276e4c3b03cfe4562a8e3ca73ef0
Purvi Murkute <purvimurkute51@gmail.com>
2025-09-03 14:14:42 +0530

    server and user model created

---
+++ server/model/User.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    profile: { type: String },
+    location: {
+      latitude: { type: Number },
+      longitude: { type: Number },
+    },
+    lastUpdated: { type: Date },
+    isLocationSharing: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
